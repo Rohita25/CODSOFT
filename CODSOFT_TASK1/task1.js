42e553057d61c224ca2c88cb42c906c00155f953
Rohita Bollam <157587643+Rohita25@users.noreply.github.com>
2024-06-19 16:40:33 +0530

    Add files via upload
    Level 1: Task 1 PORTFOLIO

---
+++ CODSOFT_TASK1/task1.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('nav a').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+  
+    document.querySelectorAll('header, section, footer').forEach(section => {
+      observer.observe(section);
+    });
+  });
+  
